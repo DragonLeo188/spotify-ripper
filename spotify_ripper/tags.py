27be0639ba14774c4c3e39f146e073a7a17360af
DragonLeo188 <59697962+DragonLeo188@users.noreply.github.com>
2021-04-25 14:23:38 +0700

    urllib => urllib.request
    isAlive => is_alive:         https://stackoverflow.com/questions/15063963/python-is-thread-still-running
    urllib => urllib.request: https://stackoverflow.com/questions/17960942/attributeerror-module-object-has-no-attribute-urlretrieve

--- spotify_ripper/tags.py
+++ spotify_ripper/tags.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import os
 import sys
 import base64
-import urllib
+import urllib.request
 
 
 def set_metadata_tags(args, audio_file, idx, track, ripper):
@@ -73,7 +73,7 @@ def set_metadata_tags(args, audio_file, idx, track, ripper):
         def get_cover_image(image_link):
             image_link = 'http://i.scdn.co%s' % (
                 image_link[len('spotify'):].replace(':', '/'))
-            cover_file = urllib.urlretrieve(image_link)[0]
+            cover_file = urllib.request.urlretrieve(image_link)[0]
 
             with open(cover_file, "rb") as f:
                 if f.mode == "rb":
